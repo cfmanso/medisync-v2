6929de98b2cf278432c9257af071f32cff0c8cd6
Carolina Manso <34281875+cfmanso@users.noreply.github.com>
2025-12-05 15:06:28 -0300

    config files

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { MedicalRecordsModule } from './medical-records/medical-records.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AppointmentsModule, MedicalRecordsModule],
   controllers: [AppController],
   providers: [AppService],
 })
